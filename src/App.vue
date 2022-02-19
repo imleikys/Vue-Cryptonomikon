358c1ee2dc1559b9b3d1e2e124d0cea89befd142
Danila Julev <daniljulev@gmail.com>
2022-02-19 14:44:06 +0300

    add(Graph) new component

--- src/App.vue
+++ src/App.vue
@@ -89,46 +89,13 @@
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
 
-      <section class="relative" v-if="selectedTicker !== null">
-        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ selectedTicker.name }} - USD
-        </h3>
-        <div class="flex items-end border-gray-600 border-b border-l h-64" ref="graph">
-          <div
-            v-for="(bar, idx) in normilizedBar"
-            :style="{ height: `${bar}%` }"
-            :key="idx"
-            class="bg-purple-800 border w-10"
-          ></div>
-        </div>
-        <button
-          type="button"
-          class="absolute top-0 right-0"
-          @click="onCloseGraph"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            xmlns:svgjs="http://svgjs.com/svgjs"
-            version="1.1"
-            width="30"
-            height="30"
-            x="0"
-            y="0"
-            viewBox="0 0 511.76 511.76"
-            style="enable-background: new 0 0 512 512"
-            xml:space="preserve"
-          >
-            <g>
-              <path
-                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                fill="#718096"
-                data-original="#000000"
-              ></path>
-            </g>
-          </svg>
-        </button>
-      </section>
+      <app-graph
+        :selectedTicker="selectedTicker"
+        :graph="graph"
+        @graph-resize="onGraphChange"
+        @close-graph="onCloseGraph"
+        v-if="selectedTicker.name !== undefined"
+      ></app-graph>
     </div>
   </div>
 </template>
@@ -136,23 +103,24 @@
 <script>
 import {subscribeToTicker, unsubscribeFromTicker} from './api';
 import AppAddTicker from './components/AppAddTicker.vue';
+import AppGraph from './components/AppGraph.vue';
 
 export default {
   name: 'App',
   components: {
     AppAddTicker,
+    AppGraph,
   },
   data() {
     return {
       loading: false,
       tickers: [],
-      selectedTicker: null,
+      selectedTicker: {},
       graph: [],
-      coins: [],
-      updateInterval: null,
       page: 1,
       filter: '',
       maxGraphElements: 1,
+      graphSize: 1,
     };
   },
 
@@ -179,19 +147,6 @@ export default {
   },
 
   computed: {
-    normilizedBar() {
-      const maxValue = Math.max(...this.graph);
-      const minValue = Math.min(...this.graph);
-
-      if (maxValue === minValue) {
-        return this.graph.map(() => 50);
-      }
-
-      return this.graph.map(
-        price => 5 + ((price - minValue) * 95) / (maxValue - minValue)
-      );
-    },
-
     filteredTickers() {
       return this.tickers.filter(ticker => ticker.name.includes(this.filter))
     },
@@ -239,6 +194,11 @@ export default {
       return price > 1 ? price.toFixed(2) : price.toPrecision(2);
     },
 
+    onGraphChange(graphSize) {
+      this.graphSize = graphSize;
+      this.calculateMaxGraphElements();
+    },
+
     updateTicker(tickerName, price) {
       this.tickers.filter(ticker => ticker.name === tickerName).forEach(
         ticker => {
@@ -255,10 +215,10 @@ export default {
     },
 
     calculateMaxGraphElements() {
-      if (!this.$refs.graph) {
+      if (this.graphSize === undefined) {
         return;
       }
-      this.maxGraphElements = this.$refs.graph.clientWidth / 38; 
+      this.maxGraphElements = this.graphSize.clientWidth / 38; 
     },
 
     onRemoveHandler(tickerToRemove) {
@@ -273,7 +233,7 @@ export default {
     },
 
     onCloseGraph() {
-      this.selectedTicker = null;
+      this.selectedTicker = {};
     },
 
     onSelectTicker(selectedTicker) {
